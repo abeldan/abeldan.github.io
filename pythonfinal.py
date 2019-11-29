aa80428241a5476b07861babc0fd353c774bbaeb
abhinayarora <57418295+abhinayarora@users.noreply.github.com>
2019-11-29 08:31:49 +0530

    Update pythonfinal.py

--- pythonfinal.py
+++ pythonfinal.py
@@ -1,22 +1,26 @@
+#Program Contributers: Abhinay Arora,Allan Beldan
+#This Program checks the Site suitability for a metro station based upon various factors such as Employment,Population,TripTime,PersonalTimeCost,Cost of trip and infrastructural Cost.
+
 import math
 
-##User Inputs##
-employment = raw_input("")
-population = raw_input("")
+##User Inputs##       Created by : Abhinay Arora
+Print "---------------------------SITE SUITABILITY ANALYSIS FOR A METRO STATION---------------------------"
+Print "---------------------------------------------------------------------------------------------------"
+employment = raw_input("Employment Around The Station: ")
+population = raw_input("Population of the City: ")
 CBDEmployment = raw_input("")
-triptime = raw_input("")
-personaltimecost = raw_input("")
-tripcost = raw_input("")
-buildingcost = raw_input("")
+triptime = raw_input("Time Consumed During Each Trip: ")
+personaltimecost = raw_input("Cost of Personal Time(per hour): ")
+tripcost = raw_input("Cost of Trip: ")
+buildingcost = raw_input("Cost of Infrastructure: ")
 
 e = math.e() #Initialize e as Euler's Numbeer
 
-##Algorithim##
+##Algorithim##  Created by : Allan Beldan
 TripsExpected = ((population+employment)*(CBDEmployment))/((triptime*personaltimecost)+tripcost)
 OutputWeightedIndex=((population+employment)*abs((employment/population)+1)^-(e^-1))/(TripsExpected/buildingcost)
-
 ##Outputs##
-
+Print ""
 
 ##Exceptions##
 except Exception:
